f8d66ca016916fd03d5b78825db308ea0ed7ac3d
koundinyag25 <koundinya1991@gmail.com>
2024-12-22 06:54:06 +0530

    fix

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -7,7 +7,7 @@ const BASE_URL = 'https://api.traceblade.xyz/api';
 
 export const sendEventToBackend = async (event: Event): Promise<void> => {
   try {
-    const response = await axios.post(`${BASE_URL}/events`, event, {
+    const response = await axios.post(`${BASE_URL}/user-events`, event, {
       headers: { 'Content-Type': 'application/json' },
     });
     console.log('Event sent successfully:', response.data);
