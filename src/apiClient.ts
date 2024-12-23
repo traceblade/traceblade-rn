c6ad203413257d0317640a265f389e2d1fbbf6b5
koundinyag25 <koundinya1991@gmail.com>
2024-12-23 05:57:01 +0530

    fix

--- src/apiClient.ts
+++ src/apiClient.ts
@@ -10,16 +10,12 @@ export const sendEventToBackend = async (
 ): Promise<void> => {
   const path = getPath(API_PATH.PUSH_EVENTS, baseUrl);
   console.log('path', path);
-  try {
-    const response = await axios.post(path, event, {
-      headers: {
-        'Content-Type': 'application/json',
-        'traceblade-api-key': event.apiKey,
-      },
-    });
-    console.log('Event sent successfully:', response.data);
-  } catch (error) {
-    console.log('error', path);
-    console.error('Failed to send event:', error);
-  }
+  console.log('event', event);
+  const response = await axios.post(path, event, {
+    headers: {
+      'Content-Type': 'application/json',
+      'traceblade-api-key': event.apiKey,
+    },
+  });
+  console.log('Event sent successfully:', response.data);
 };
