379050156775daafe383d7f77ff6a2918e6217d6
koundinyag25 <koundinya1991@gmail.com>
2024-12-23 08:16:55 +0530

    adding crypto fix

---
+++ src/helper/user.ts
@@ -0,0 +1,16 @@
+import * as Crypto from 'expo-crypto';
+
+const generateUserId = async () => {
+  // Generate 16 random bytes
+  const randomBytes = await Crypto.getRandomBytesAsync(16);
+
+  // Convert the random bytes to a hexadecimal string
+  const userId = Array.from(randomBytes)
+    .map((byte) => byte.toString(16).padStart(2, '0')) // Convert byte to hex
+    .join(''); // Join all bytes into a single string
+
+  console.log('Generated User ID:', userId);
+  return userId;
+};
+
+generateUserId();
--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { AppState, AppStateStatus } from 'react-native';
 import { processEventQueue, queueEvent } from './eventQueue';
 import { sendEventToBackend } from './apiClient';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import 'react-native-get-random-values';
+
 import { v4 as uuidv4 } from 'uuid';
 import { BASE_URL } from './constants/api';
 
